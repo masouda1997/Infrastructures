d815b4d65e88ad6c374d052ee93c978ef48df726
Masoud anaraki <masoud30376@gmail.com>
2025-05-17 13:04:26 +0330

    add husky and sahdcn in the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from '@/components/ui/button';
+import { Library } from 'lucide-react';
+
 import React from 'react';
 
 export default function Home() {
 	console.log('this it a log');
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			this is test
+			<Button variant={"outline"} ><Library /> ShadCN</Button>
 		</div>
 	);
 }
